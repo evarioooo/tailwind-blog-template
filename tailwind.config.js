e2e6887c19dda566b120cc8c5faa884ff3aafc3c
evarioooo <alxndr.wlfgng.brgmnn@gmail.com>
2024-07-28 10:18:55 +0000

    28. Juli 2024, 12:18

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ const plugin = require('tailwindcss/plugin');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./**/*.{html,js}'],
+  content: ['./src/**/*.{html,js}'],
   theme: {
     extend: {
       colors: {
